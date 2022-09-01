6bd27c1a325802e49ccacf58611672ea1fb8ccbd
Improve <1394798608@qq.com>
2022-09-01 18:06:22 +0800

    format src

--- src/pages/Welcome.tsx
+++ src/pages/Welcome.tsx
@@ -5,45 +5,46 @@ import React from 'react';
 import styles from './Welcome.less';
 
 const CodePreview: React.FC = ({ children }) => (
-  <pre className={styles.pre}>
-    <code>
-      <Typography.Text copyable>{children}</Typography.Text>
-    </code>
-  </pre>
+    <pre className={styles.pre}>
+        <code>
+            <Typography.Text copyable>{children}</Typography.Text>
+        </code>
+    </pre>
 );
 
 const Welcome: React.FC = () => {
-  const intl = useIntl();
+    const intl = useIntl();
 
-  return (
-    <PageContainer>
-      <Card>
-        <Alert
-          message={intl.formatMessage({
-            id: 'pages.welcome.alertMessage',
-            defaultMessage: 'Faster and stronger heavy-duty components have been released.',
-          })}
-          type="success"
-          showIcon
-          banner
-          style={{
-            margin: -12,
-            marginBottom: 24,
-          }}
-        />
-        <Typography.Text strong>
-          <a
-            href="https://procomponents.ant.design/components/table"
-            rel="noopener noreferrer"
-            target="__blank"
-          >
-            <FormattedMessage id="pages.welcome.link" defaultMessage="Welcome" />
-          </a>
-        </Typography.Text>
-        <CodePreview>yarn add @ant-design/pro-components</CodePreview>
-      </Card>
-    </PageContainer>
-  );
+    return (
+        <PageContainer>
+            <Card>
+                <Alert
+                    message={intl.formatMessage({
+                        id: 'pages.welcome.alertMessage',
+                        defaultMessage:
+                            'Faster and stronger heavy-duty components have been released.',
+                    })}
+                    type="success"
+                    showIcon
+                    banner
+                    style={{
+                        margin: -12,
+                        marginBottom: 24,
+                    }}
+                />
+                <Typography.Text strong>
+                    <a
+                        href="https://procomponents.ant.design/components/table"
+                        rel="noopener noreferrer"
+                        target="__blank"
+                    >
+                        <FormattedMessage id="pages.welcome.link" defaultMessage="Welcome" />
+                    </a>
+                </Typography.Text>
+                <CodePreview>yarn add @ant-design/pro-components</CodePreview>
+            </Card>
+        </PageContainer>
+    );
 };
 
 export default Welcome;
